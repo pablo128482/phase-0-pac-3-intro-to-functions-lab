ed351aa37c3934e01e3e888d42e8022784787ff5
pablo128482 <ashley.c.clay@gmail.com>
2023-05-17 18:31:30 -0700

    final

---
+++ index.js
@@ -0,0 +1,57 @@
+function shout(string) {
+    // todo
+  }
+
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string) {
+    // todo
+  }
+
+  function whisper(string) {
+    return string;
+  }
+  "Hello!".toLowerCase(); // 'HELLO!'
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+ 
+
+  const string = "Hello!";
+
+  console.log(string);
+  console.log(string.toUpperCase());
+  
+  console.log(string === string.toUpperCase());
+
+  function logShout(string) {  
+  
+    console.log(string.toUpperCase())  
+      
+    }
+    console.log(string === string.toLowerCase());
+
+    function logWhisper(string) {
+      console.log(string.toLowerCase())
+
+    }
+
+  
+function sayHiToHeadphonedRoommate(string){
+if (string.toLowerCase() === string){
+return "I can't hear you!"
+}
+if (string.toUpperCase() === string){
+return "YES INDEED!"
+}
+if (string === "Let's have dinner together!"){
+return "I would love to!"
+}}
